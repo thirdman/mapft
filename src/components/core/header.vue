17402d87f49b1943409c365b5135fe43a5944f2a
Gareth Bedford <g@thirdman.co.nz>
2020-09-23 08:44:32 +0700

    Initial demo fork

--- src/components/core/header.vue
+++ src/components/core/header.vue
@@ -1,21 +1,21 @@
 <template>
-    <v-toolbar color="primary" app>
-        <v-toolbar-items class="hidden-xs-only">
-            <v-btn text to="/" color="white">
-                <v-icon left>mdi-home</v-icon>Home
-            </v-btn>
-            <v-btn text to="/account" color="white" v-if="loggedIn">
-                <v-icon left>mdi-account</v-icon>Account
-            </v-btn>
-        </v-toolbar-items>
-        <v-spacer />
-        <v-btn text color="white" v-if="loggedIn" @click="fcl.unauthenticate">
-            <v-icon left>mdi-logout</v-icon>Logout
-        </v-btn>
-        <v-btn text color="white" v-else @click="fcl.authenticate">
-            <v-icon left>mdi-login</v-icon>Login
-        </v-btn>
-    </v-toolbar>
+  <v-toolbar color="transparent" app>
+    <v-toolbar-items class="hidden-xs-only">
+      <v-btn text to="/" color="primary">
+        <v-icon left>mdi-home</v-icon>Home
+      </v-btn>
+      <v-btn text to="/account" color="primary" v-if="loggedIn">
+        <v-icon left>mdi-account</v-icon>Account
+      </v-btn>
+    </v-toolbar-items>
+    <v-spacer />
+    <v-btn text color="primary" v-if="loggedIn" @click="fcl.unauthenticate">
+      <v-icon left>mdi-logout</v-icon>Logout
+    </v-btn>
+    <v-btn text color="primary" v-else @click="fcl.authenticate">
+      <v-icon left>mdi-login</v-icon>Login
+    </v-btn>
+  </v-toolbar>
 </template>
 
 <script>
@@ -23,13 +23,13 @@ import * as fcl from "@onflow/fcl";
 import { mapGetters } from "vuex";
 
 export default {
-    data() {
-        return {
-            fcl,
-        };
-    },
-    computed: {
-        ...mapGetters(["loggedIn"]),
-    },
+  data() {
+    return {
+      fcl,
+    };
+  },
+  computed: {
+    ...mapGetters(["loggedIn"]),
+  },
 };
 </script>
