e34fb1555b3e6382ef4036d8cdc1999ad41e9990
Gareth Bedford <g@thirdman.co.nz>
2021-07-23 13:36:35 +0700

    update packages, refactor to allow for legacy node-sass

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,11 @@ module.exports = {
   outputDir: "docs",
   transpileDependencies: ["vuetify"],
   // "lintOnSave": false
+  // css: {
+  //   loaderOptions: {
+  //     sass: {
+  //       prependData: `@import "@/sass/variables.sass"`,
+  //     },
+  //   },
+  // },
 };
