903730e537e6d9456de85f8d5adbbb84b9245bb6
Gareth Bedford <g@thirdman.co.nz>
2021-08-18 14:59:59 +0700

    try again

--- nuxt.config.js
+++ nuxt.config.js
@@ -194,6 +194,7 @@ export default {
     babel: {
       plugins: [["@babel/plugin-proposal-private-methods", { loose: true }]],
     },
+
     /*
      ** You can extend webpack config here
      */
